c767fd2030f78b8867b6bb020b2cc4515a61837d
sophiarollins <sophiarollins@Sophias-MacBook-Pro.local>
2022-01-06 17:12:55 -0600

    write to firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 
 import './App.css';
 import CourseList from './components/CourseList';
+import { timeParts } from './components/Course';
 import { useData } from './utilities/firebase.js';
 
 
 const Banner = ({ title }) => (
   <h1>{title}</h1>
 );
-
-const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
-
-const timeParts = meets => {
-  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
-  return !match ? {} : {
-    days,
-    hours: {
-      start: hh1 * 60 + mm1 * 1,
-      end: hh2 * 60 + mm2 * 1
-    }
-  };
-};
 
 const mapValues = (fn, obj) => (
   Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
--- src/components/Course.js
+++ src/components/Course.js
@@ -1,4 +1,5 @@
 import { hasConflict, getCourseTerm } from '../utilities/times.js';
+import { setData } from '../utilities/firebase.js';
 
 const toggle = (x, lst) => (
     lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
@@ -7,6 +8,37 @@ const toggle = (x, lst) => (
 const getCourseNumber = course => (
     course.id.slice(1, 4)
 );
+
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+export const timeParts = meets => {
+    const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+    return !match ? {} : {
+        days,
+        hours: {
+            start: hh1 * 60 + mm1 * 1,
+            end: hh2 * 60 + mm2 * 1
+        }
+    };
+};
+
+const reschedule = async (course, meets) => {
+    if (meets && window.confirm(`Change ${course.id} to ${meets}?`)) {
+        try {
+            await setData(`/courses/${course.id}/meets`, meets);
+        } catch (error) {
+            alert(error);
+        }
+    }
+};
+
+const getCourseMeetingData = course => {
+    const meets = prompt('Enter meeting data: MTuWThF hh:mm-hh:mm', course.meets);
+    const valid = !meets || timeParts(meets).days;
+    if (valid) return meets;
+    alert('Invalid meeting data');
+    return null;
+};
 
 const Course = ({ course, selected, setSelected }) => {
     const isSelected = selected.includes(course);
@@ -17,7 +49,8 @@ const Course = ({ course, selected, setSelected }) => {
     return (
         <div className="card m-1 p-2"
             style={style}
-            onClick={isDisabled ? null : () => setSelected(toggle(course, selected))}>
+            onClick={isDisabled ? null : () => setSelected(toggle(course, selected))}
+            onDoubleClick={() => reschedule(course, getCourseMeetingData(course))}>>
             <div className="card-body">
                 <div className="card-title">{getCourseTerm(course)} CS {getCourseNumber(course)}</div>
                 <div className="card-text">{course.title}</div>
