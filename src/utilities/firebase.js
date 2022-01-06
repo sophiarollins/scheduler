e5f4ce724d0b0bd0886e51a29598f0169df1c18f
sophiarollins <sophiarollins@Sophias-MacBook-Pro.local>
2022-01-06 17:01:35 -0600

    read from firebase

---
+++ src/utilities/firebase.js
@@ -0,0 +1,52 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import React, { useState, useEffect } from 'react';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBPTSQKhIMn2FpBbjMzAPkjXUetg_qx74A",
+    authDomain: "scheduler-1c419.firebaseapp.com",
+    databaseURL: "https://scheduler-1c419-default-rtdb.firebaseio.com",
+    projectId: "scheduler-1c419",
+    storageBucket: "scheduler-1c419.appspot.com",
+    messagingSenderId: "560583202567",
+    appId: "1:560583202567:web:8a290b4d774d76313797a2",
+    measurementId: "G-46S2ZXLMER"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        const dbRef = ref(database, path);
+        const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+        if (devMode) { console.log(`loading ${path}`); }
+        return onValue(dbRef, (snapshot) => {
+            const val = snapshot.val();
+            if (devMode) { console.log(val); }
+            setData(transform ? transform(val) : val);
+            setLoading(false);
+            setError(null);
+        }, (error) => {
+            setData(null);
+            setLoading(false);
+            setError(error);
+        });
+    }, [path, transform]);
+
+    return [data, loading, error];
+};
